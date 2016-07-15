d817d7d15806274ba90e50034c3c7d855de8d61b
Andrew Baik <and.baik@gmail.com>
2016-07-15 19:07:22 -0400

    1.07

--- lib/ch01/1.07.rb
+++ lib/ch01/1.07.rb
@@ -1,1 +1,42 @@
 # Rotate Matrix
+=begin
+Basically, for an N x N matrix
+Start top left of each "layer" (from the outermost layer)
+and do a 4-swap for the first M - 1 elements,
+where M is the length of top row of the layer
+For example:
+
+[
+  [1,2,3]
+  [4,5,6]
+  [7,8,9]
+]
+
+Move 1 to 3, 3 to 9, 9 to 7, 7 to 1
+Then do the same 4-way swap for element 2 within the same layer.
+The layer should be fully rotated.
+=end
+
+
+def rotate_matrix(matrix)
+  n = matrix.length
+
+  (n/2).times do |layer|
+    final = n - layer - 1
+    (layer..final-1).each do |i|
+      offset = i - layer
+      # save top
+      top = matrix[layer][i]
+      # left to top
+      matrix[layer][i] = matrix[final - offset][layer]
+      # bottom to left
+      matrix[final - offset][layer] = matrix[final][final - offset]
+      # right to bottom
+      matrix[final][final - offset] = matrix[i][final]
+      # top to right
+      matrix[i][final] = top
+    end
+  end
+
+  return matrix
+end
--- spec/ch01/1.07_spec.rb
+++ spec/ch01/1.07_spec.rb
@@ -2,6 +2,14 @@ require "ch01/1.07.rb"
 
 describe "1.7 Rotate Matrix" do
 
+  it 'rotates [[1,2], [3,4]] to [[3,1], [4,2]] 90 deg clockwise' do
+    expect(rotate_matrix([[1,2], [3,4]])).to eq([[3,1], [4,2]])
+  end
 
+  it 'rotates [[1,2,3], [4,5,6], [7,8,9]] to [[3,1], [4,2]] 90 deg clockwise' do
+    actual = rotate_matrix([[1,2,3], [4,5,6], [7,8,9]])
+    expected = [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+    expect(actual).to eq(expected)
+  end
 
 end
