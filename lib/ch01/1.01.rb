18f42f75e0e54584d4c129007f126d31eacdfe65
Andrew Baik <and.baik@gmail.com>
2016-07-15 00:29:04 -0400

    1.01

--- lib/ch01/1.01.rb
+++ lib/ch01/1.01.rb
@@ -1,1 +1,10 @@
 # Is Unique
+
+def unique_chars?(str)
+  char_set = Hash.new(0)
+  str.length.times do |i|
+    return false if char_set[str[i]] == 1
+    char_set[str[i]] += 1
+  end
+  return true
+end
--- spec/ch01/1.01_spec.rb
+++ spec/ch01/1.01_spec.rb
@@ -1,4 +1,38 @@
-require 'ch01/1.01.rb'
+require "ch01/1.01.rb"
 
-describe '1.1 Is Unique' do
+describe "1.1 Is Unique" do
+
+  describe "brute force solution" do
+
+    it "returns true for input string 'abc'" do
+      expect(unique_chars?('abc')).to eq(true)
+    end
+
+    it "returns false for input string 'abca'" do
+      expect(unique_chars?('abca')).to eq(false)
+    end
+
+    it "returns false for input string 'aaa'" do
+      expect(unique_chars?('aaa')).to eq(false)
+    end
+
+    it "returns true for abc...zABC...Z" do
+      lower = 'abcdefghijklmnopqrstuvwxyz'
+      expect(unique_chars?(lower + lower.upcase)).to eq(true)
+    end
+
+    it "returns true for abc...zABC...ZZ" do
+      lower = 'abcdefghijklmnopqrstuvwxyz'
+      expect(unique_chars?(lower + lower.upcase + "Z")).to eq(false)
+    end
+
+    it "returns true for 123" do
+      expect(unique_chars?('123')).to eq(true)
+    end
+
+    it "returns false for 111" do
+      expect(unique_chars?('111')).to eq(false)
+    end
+  end
+
 end
