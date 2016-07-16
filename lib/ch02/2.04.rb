1d622acb819a048afe718e4af010d4a763a4b87a
Andrew Baik <and.baik@gmail.com>
2016-07-16 18:46:57 -0400

    2.04

--- lib/ch02/2.04.rb
+++ lib/ch02/2.04.rb
@@ -1,34 +1,53 @@
 # Partition
 
-Node = Struct.new(:next, :data)
-# linked_list is assumed to be a list of Nodes
-# connected via the .next reference
-# please refer to spec/ch02/2.04_spec.rb file to see how
-# the linked list is setup
+Node = Struct.new(:data, :next)
 
-def partition(node, val)
-  lo = Node.new(nil, nil)
-  hi = Node.new(nil, nil)
-  lo_head = lo
-  hi_head = hi
+class LinkedList
+  attr_reader :head, :tail
 
-  while node
-    if node >= val
-      hi = add_node(hi, val)
-    elsif node < val
-      lo = add_node(lo, val)
+  def initialize(values=[])
+    @head = values.empty? ? Node.new(nil, nil) : Node.new(values.first, nil)
+    @tail = @head
+    values[1..-1].each { |v| append(v) } if values.length > 1
+  end
+
+  def append(value)
+    if tail.data
+      @tail.next = Node.new(value, nil)
+    else
+      @tail.data = value
+    end
+    @tail = @tail.next if @tail.next
+  end
+
+  def +(linked_list)
+    @tail.next = linked_list.head
+    @tail = linked_list.tail
+    return self
+  end
+
+  def print_list
+    current = @head
+    while current
+      # print "#{current.data} "
+      current = current.next
     end
   end
-
-  lo.next = hi_head
-  return lo_head
 end
 
-def add_node(current_node, val)
-  if current_node.data
-    current_node.next = Node.new(nil, node.data)
-  else
-    current_node.data = val
+def partition(linked_list, val)
+  lo = LinkedList.new
+  hi = LinkedList.new
+  current = linked_list.head
+
+  while current
+    if current.data >= val
+      hi.append(current.data)
+    else
+      lo.append(current.data)
+    end
+    current = current.next
   end
-  return current_node
+
+  return lo + hi
 end
--- spec/ch02/2.04_spec.rb
+++ spec/ch02/2.04_spec.rb
@@ -2,15 +2,31 @@ require "ch02/2.04.rb"
 
 describe "2.4 Partition" do
 
-  let(:linked_list) do
-    nodes = (1..5).to_a.inject([]) { |m, v| m << Node.new(nil, v) }
-    nodes.each_with_index { |node, i| node.next = nodes[i + 1] }
-    return nodes[0]
+  let(:node) { Node.new(5, nil) }
+  let(:linked_list) { LinkedList.new([5,4,3,2,1]) }
+
+  it "5 > 4 > 3 > 2 > 1 partition 3 should be 2 > 1 > 5 > 4 > 3" do
+    actual = partition(linked_list, 3)
+    expected = LinkedList.new([2,1,5,4,3])
+    expect(actual.print_list).to eq expected.print_list
   end
 
-  it "5 > 4 > 3 > 2 > 1 partition 3 should be 2 > 1 > 5 > 4 > 3"
-  it "5 > 4 > 3 > 2 > 1 partition 5 should be 4 > 3 > 2 > 1 > 5"
-  it "5 > 5 > 4 > 6 partition 5 should be 4 > 5 > 5 > 6"
-  it "1 > 2 > 3 partition 1 should be same"
+  it "5 > 4 > 3 > 2 > 1 partition 5 should be 4 > 3 > 2 > 1 > 5" do
+    actual = partition(linked_list, 5)
+    expected = LinkedList.new([4,3,2,1,5])
+    expect(actual.print_list).to eq expected.print_list
+  end
+
+  it "5 > 5 > 4 > 6 partition 5 should be 4 > 5 > 5 > 6" do
+    actual = partition(LinkedList.new([5,5,4,6]), 5)
+    expected = LinkedList.new([4,5,5,6])
+    expect(actual.print_list).to eq expected.print_list
+  end
+
+  it "1 > 2 > 3 partition 1 should be same" do
+    actual = partition(LinkedList.new([1,2,3]), 1)
+    expected = LinkedList.new([1,2,3])
+    expect(actual.print_list).to eq expected.print_list
+  end
 
 end
