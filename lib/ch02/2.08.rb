d7d1ecc4f9743e2769b299067d8db0f800bbbec7
Andrew Baik <and.baik@gmail.com>
2016-07-17 22:51:41 -0400

    2.08

--- lib/ch02/2.08.rb
+++ lib/ch02/2.08.rb
@@ -1,5 +1,19 @@
+require_relative "linked_list.rb"
+
 # Loop Detection
 
 def loop_detection(linked_list)
+  node_hash = Hash.new(0)
+  current = linked_list.head
 
+  while current
+    if node_hash[current] == 0
+      node_hash[current] += 1
+    else
+      return current
+    end
+    current = current.next
+  end
+
+  return nil
 end
--- spec/ch02/2.08_spec.rb
+++ spec/ch02/2.08_spec.rb
@@ -2,11 +2,18 @@ require "ch02/2.08.rb"
 
 describe "2.8 Loop Detection" do
   let(:one) { LinkedList.new([1,2,3]) }
-  let(:two) { LinkedList.new([4,5,6]) }
-  let(:thr) { LinkedList.new([9,9,9]) }
+  let(:circular) do
+    list = LinkedList.new([1,2,3,4,5,6,7,8,9,10])
+    list.tail.next = list.head
+    return list
+  end
 
-  it "detects circular loop"
+  it "detects circular loop" do
+    expect(loop_detection(circular)).to eq circular.head
+  end
 
-  it ""
+  it "returns nil for noncircular loop" do
+    expect(loop_detection(one)).to eq nil
+  end
 
 end
