e5ad8569a8decc88ec49bd8e7a641ed5813e19fa
Andrew Baik <and.baik@gmail.com>
2016-07-17 23:07:07 -0400

    refactored

--- lib/ch02/2.08.rb
+++ lib/ch02/2.08.rb
@@ -3,14 +3,14 @@ require_relative "linked_list.rb"
 # Loop Detection
 
 def loop_detection(linked_list)
-  node_hash = Hash.new(0)
+  node_hash = {}
   current = linked_list.head
 
   while current
-    if node_hash[current] == 0
-      node_hash[current] += 1
-    else
+    if node_hash[current]
       return current
+    else
+      node_hash[current] = true
     end
     current = current.next
   end
