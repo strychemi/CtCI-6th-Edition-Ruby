8c30cfd99905365c8d29dfbcd5ad7a57560d2ce4
Percy Grunwald <percy.grunwald@gmail.com>
2019-04-25 21:15:47 +0200

    Updates spec and fixes solution for problem 1.05

--- lib/ch01/1.05.rb
+++ lib/ch01/1.05.rb
@@ -1,18 +1,36 @@
 # One Away
 
 def one_away(init, final)
-  letter_diff = Hash.new(0)
+  return true if init == final
 
-  if (init.length - final.length).abs > 1
-    return false
-  else
-    init.length.times { |i| letter_diff[init[i]] += 1 }
-    final.length.times { |i| letter_diff[init[i]] -= 1 }
-    letter_diff.keys.each do |letter|
-      letter_diff.delete(letter) if letter_diff[letter] == 0
+  length_difference = init.length - final.length
+  length_difference_abs = length_difference.abs
+
+  if length_difference_abs == 0
+    # must be one away by replacement
+    letter_diff_count = 0.upto(init.length).count {|i| init[i] != final[i]}
+    letter_diff_count <= 1
+  elsif length_difference_abs == 1
+    # must be one away by insertion
+    shortest_string = length_difference < 0 ? init : final
+    longest_string = length_difference > 0 ? init : final
+    short_index = 0
+    long_index = 0
+
+    while short_index < shortest_string.length && long_index < longest_string.length
+      if shortest_string[short_index] == longest_string[long_index]
+        short_index += 1
+        long_index += 1
+      elsif short_index == long_index
+        # skip one letter in longest string the first time chars don't match
+        long_index += 1
+      else
+        return false
+      end
     end
-    return false if letter_diff.length > 1
-  end
 
-  return true
+    true
+  else
+    false
+  end 
 end
