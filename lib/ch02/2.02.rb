92302c39bce575484feb06e73c628777b3d70e41
Nathan Worden <34219544+nathanworden@users.noreply.github.com>
2020-11-19 15:53:11 -0800

    Update 2.02.rb

--- lib/ch02/2.02.rb
+++ lib/ch02/2.02.rb
@@ -3,9 +3,9 @@ require_relative "linked_list.rb"
 
 def kth_last(linked_list, k)
   # r1 is spaced k-1 nodes ahead of r2
-  r1 = linked_list
+  r1 = linked_list.head
   (k-1).times { r1 = r1.next }
-  r2 = linked_list
+  r2 = linked_list.head
 
   # when r1 hits the end of the list, the kth should be at r2
   while r1.next
@@ -13,5 +13,10 @@ def kth_last(linked_list, k)
     r2 = r2.next
   end
 
-  return r2
+  return r2.data
 end
+
+list = LinkedList.new(["once", "upon", "a", "time", "there", "was", "a", "boy", "named", "Nathan"])
+
+p list.to_str
+p kth_last(list, 2)  # => named
